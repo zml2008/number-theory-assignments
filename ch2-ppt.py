0484f804eafda6bd01b53e4fd917a1d572a1d508
zml <zml@aoeu.xyz>
2015-01-27 13:34:51 -0800

    Efficiency tweaks, add in gitignore

--- ch2-ppt.py
+++ ch2-ppt.py
@@ -1,12 +1,15 @@
 __author__ = 'zml'
 
+
 from util import gathering_input, factors
 from multiprocessing import Pool
 import os
 import itertools
 
+
 def a(s, t):
     return s * t
+
 
 def b(s, t):
     return ((s * s) - (t * t))/2
@@ -19,9 +22,10 @@ def c(s, t):
 def _input_func(s):
     rets = []
     for t in range(1, s, 2):
-        if len((factors(s) & factors(t)) - {1}) == 0:
+        if factors(s) & factors(t) == {1}:
             rets.append((a(s, t), b(s, t), c(s, t)))
     return rets
+
 
 @gathering_input("Maximum s value: ")
 def generate_ppts(max_s):
@@ -30,6 +34,7 @@ def generate_ppts(max_s):
     print(len(values), "pythagorean triples: ")
     for triple in values:
         print("%d² + %d² = %d²" % triple)
+
 
 if __name__ == "__main__":
     generate_ppts()
