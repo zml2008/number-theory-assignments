caaa2a4d1afe8a9aa2fa7f11515b5980015c8e79
zml <zml@aoeu.xyz>
2015-01-30 13:54:47 -0800

    Initial implementation of chapter 3's PPT algorithm

---
+++ ch3-ppt-simple.py
@@ -0,0 +1,31 @@
+def a(u, v):
+    return u * u - v * v
+
+
+def b(u, v):
+    return 2 * v * u
+
+
+def c(u, v):
+    return u * u + v * v
+
+
+def no_shared_factors(s, t):
+    for i in range(2, min(s, t) + 1):
+        if s % i == 0 and t % i == 0:
+            return False
+    return True
+
+
+def print_ppts():
+    max_u = int(input("Maximum u value: "))
+    count = 0
+    for u in range (1, max_u + 1):
+        for v in range(1, u):
+            if no_shared_factors(u, v) and (u - v) % 2 != 0:
+                count += 1
+                print((u, v, a(u, v), b(u, v), c(u, v)))
+    print("Total number of PPTs:", count)
+
+if __name__ == "__main__":
+    print_ppts()
