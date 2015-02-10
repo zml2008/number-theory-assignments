1f11e8b6ae30957dbc86c280c3be67c84da793bc
zml <zml@aoeu.xyz>
2015-02-10 08:02:13 -0800

    LCM things

--- ch5.py
+++ ch5.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 __author__ = 'zml'
 
-import fractions
 def gcd(a, b):
     low = min(a, b)
     high = max(a, b)
@@ -9,7 +8,18 @@ def gcd(a, b):
         high, low = low, high % low
     return high
 
+def lcm(a, b):
+    return (a * b) / gcd(a, b)
+
+def another_lcm(a, b):
+    low = min(a, b)
+    high = max(a, b)
+    while low != 0:
+        high, low = (a*b)/low, high % low
+    return high
 
 if __name__ == "__main__":
+    print(lcm(20, 30))
+    print(another_lcm(20, 30))
     print(gcd(36, 132))
     print(gcd(12345, 67890))
