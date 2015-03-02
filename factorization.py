9259f26f15be6dfd102d5f1e143e42e0dddebec3
zml <zml@aoeu.xyz>
2015-03-02 08:19:49 -0800

    Actually commit prime factorization script

---
+++ factorization.py
@@ -0,0 +1,48 @@
+__author__ = 'zml'
+
+import math
+
+
+def is_prime(num):
+    for i in range(2, math.ceil(math.sqrt(num))):
+        if num % i == 0 and num != i:
+            return False
+    return True
+
+
+def primes(start=1, primelimit=1000):
+    finals = 0
+    last = start
+    while finals < primelimit:
+        last += 1
+        found = True
+        for i in range(2, math.ceil(math.sqrt(last))):
+            if last % i == 0 and last != i:
+                found = False
+        if found:
+            yield last
+            finals += 1
+
+
+def prime_factorization(num):
+    primelimit = 100
+    last_prime = 1
+    while not is_prime(num):
+        for i in primes(last_prime, primelimit):
+            if i == 1:
+                continue
+            while num % i == 0:
+                yield i
+                num //= i
+            last_prime = i
+
+    if not is_prime(num):
+        print("Not actually prime!")
+
+    if num != 1:
+        yield num
+
+
+if __name__ == "__main__":
+    n = int(input("Prime factorization of integer n="))
+    print("n=%d" % n, list(prime_factorization(n)))
