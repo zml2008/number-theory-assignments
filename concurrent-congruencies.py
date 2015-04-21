00c7d15e5d9fed51bb86b9e96ed8b15150f0ab38
zml <zml@aoeu.xyz>
2015-04-20 23:22:26 -0700

    Clean up concurrent congruencies solver to be more friendly to programatic usage

--- concurrent-congruencies.py
+++ concurrent-congruencies.py
@@ -2,8 +2,11 @@ __author__ = 'zml'
 
 from congruence import congruencies, parse_congruency
 
-def main(args):
+
+def solve(*congrs):
     """
+
+    :type congrs: list
     This contains an algorithm for solving an arbitrary number of concurrent congruencies, like so:
     1. initialize p_m to 1 and p_c to 0. define congrs to be a list of 3-tuples (a, c, m) representing a{congr}cmodm
     2. set i = 0
@@ -16,9 +19,11 @@ def main(args):
     9. increment i by 1 and go to step 2
 
     :param args: a series of congruencies in string form
-    :return: nada, we print
+    :return: the x value that solves every input congruence
     """
-    congrs = [parse_congruency(arg) for arg in args]
+    if len(congrs) == 0:
+        raise BaseException("No input values provided!")
+
     p_m, p_c = 1, 0
     for i in range(len(congrs)):
         a, c, m = congrs[i]
@@ -28,12 +33,16 @@ def main(args):
         if len(solns) != 1:
             raise BaseException("Invalid length of solutions -- expected 1 but got %d" % len(solns))
         c = solns[0] % m
-        if i != len(congrs) - 1:
-            p_c += p_m * c
-            p_m *= m
-        else:
-            print("Solution is %d*%d + %d=%d" % (p_m, c, p_c, p_m * c + p_c))
+        if i == len(congrs) - 1:
+            return p_m * c + p_c
+        p_c += p_m * c
+        p_m *= m
 
+
+def main(args):
+    congrs = [parse_congruency(arg) for arg in args]
+    ret = solve(*congrs)
+    print("Solution is x=%d" % ret)
 
 if __name__ == "__main__":
     import sys
