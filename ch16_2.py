8202fd054b5ce19a4e2de41fc9aebc13c96a34cf
zml <zml@aoeu.xyz>
2015-05-05 13:35:23 -0700

    Add implementation of successive squaring

---
+++ ch16_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+__author__ = 'zml'
+
+def successive_square(a, k, m):
+    b = 1
+    while k >= 1:
+        if k % 2 == 1:
+            b = (a*b) % m
+        a = (a * a) % m
+        k //= 2
+    return b
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Calculate the value of a^k mod m")
+    parser.add_argument("a", type=int)
+    parser.add_argument("k", type=int)
+    parser.add_argument("m", type=int)
+    args = parser.parse_args()
+
+    print("%d^%d mod %d=%d" % (args.a, args.k, args.m, successive_square(args.a, args.k, args.m)))
